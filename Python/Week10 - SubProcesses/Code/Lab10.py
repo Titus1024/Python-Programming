21ffc1ae8170878c64cc4df81c3f8fbc8b466cf6
kali <kali@kali-python.seclan.local>
2020-11-12 11:44:05 -0600

    Completed               lab.

--- Python/Week10 - SubProcesses/Code/Lab10.py
+++ Python/Week10 - SubProcesses/Code/Lab10.py
@@ -1,5 +1,14 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python3
 import subprocess
 def GetProcess():
     result = subprocess.run(['ps','-axco','command'],stdout=subprocess.PIPE)
-    return result.stdout.decode.split()
+    return result
+MyProc = GetProcess()
+MyProcString = MyProc.stdout.decode()
+MyProcList = MyProc.stdout.decode().split('\n')[1:]
+print(type(MyProc))
+print(type(MyProcString))
+print(type(MyProcList))
+print(len(MyProcList))
+for Line in MyProcList:
+    print(Line)
